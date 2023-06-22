d4e7f96b1b70c6c51de417c25f0ceae9ca4e9fdc
prashantnair <prashantnair1999@gmail.com>
2023-06-23 01:37:19 +0530

    first commit

--- Server/app.js
+++ Server/app.js
@@ -5,7 +5,6 @@ const app = express()
 const Port = process.env.Port || 4000
 const ExpenseRoutes = require("../Routes/Expense")
 const ConnectDB = require("../Connection/conn")
-const ExpenseModel = require("../Model/ExpenseModel")
 
 
 app.use(express.json())
@@ -13,12 +12,11 @@ app.use(cors())
 
 app.use("/api/Expense",ExpenseRoutes)
 
-app.get("/",async(req,res)=>{
-    const showfuction = await ExpenseModel.find({})
+app.get("/",(req,res)=>{
+    
     res.json({
        name:"Expense Tracker",
-       msg:"let me see you can do it or not",
-       data:showfuction
+       msg:"let me see you can do it or not"
     })
    })
    
