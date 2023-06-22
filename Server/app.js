9eb12da34fe0f7766a79ce8630731d850d33c150
prashantnair <prashantnair1999@gmail.com>
2023-06-23 01:41:31 +0530

    first commit

--- Server/app.js
+++ Server/app.js
@@ -12,14 +12,34 @@ app.use(cors())
 
 app.use("/api/Expense",ExpenseRoutes)
 
-app.get("/",(req,res)=>{
+app.get("/",async(req,res)=>{
     
     res.json({
        name:"Expense Tracker",
        msg:"let me see you can do it or not"
     })
    })
-   
+
+   app.get("/Show",async(req,res)=>{
+    try{
+        const ShowExpense = await ExpenseModel.find({Email:req.body.Email})
+        res.json({
+            data:ShowExpense,
+            name:"Expense Tracker Added",
+            msg:"let me see you can do it or not"
+         })
+
+    }
+    catch(e){
+       console.log(e)
+       res.status(201).json({
+        Success:false,
+        msg:"Expense Tracker Added failed"
+       })
+    }
+    
+   })
+
 
 app.listen(Port,()=>{
     ConnectDB()
